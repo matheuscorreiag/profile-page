dbf1380542afb20dcdce97ff0e0754664d7e8fca
Matheus Correia <matheuscorreiags@gmail.com>
2025-02-06 11:13:34 -0300

    wip: timeline order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import { ProfileSection } from "@/components/custom/profile-section";
+import { Timeline } from "@/components/custom/timeline";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
       <div
         className="flex w-full h-full text-white pt-28 flex-col
-                items-center justify-center px-page flex-1 px-page border-b border-zinc-800 py-16 xl:py-32"
+                items-center justify-center flex-1 border-b border-zinc-800 py-16 xl:py-32 px-page"
       >
         <ProfileSection />
       </div>
 
-      <div className="flex flex-col text-white px-page py-16 xl:py-32 bg-neutral-950">
-        <h1 className="whitespace-nowrap text-center text-3xl font-bold leading-tight lg:text-[3rem] lg:leading-normal">
-          Timeline
-        </h1>
-      </div>
+      <Timeline />
     </div>
   );
 }
---
+++ src/components/custom/timeline.tsx
@@ -0,0 +1,34 @@
+import { WorkDetails } from "@/components/custom/work-details";
+
+export function Timeline() {
+  return (
+    <div className="flex flex-col text-white py-16 xl:py-32 bg-neutral-950">
+      <h1 className="whitespace-nowrap text-center text-3xl font-bold leading-tight lg:text-[3rem] lg:leading-normal">
+        Timeline
+      </h1>
+
+      <div className="space-y-8 lg:flex-row lg:gap-x-16 lg:gap-y-0 grid grid-cols-1 place-items-center px-page mt-24 relative">
+        <WorkDetails
+          startYear={2022}
+          endYear={null}
+          jobTitle="Backend Developer in a Angola delivery company (current)"
+          companyName="Mamboo"
+        />
+
+        <WorkDetails
+          startYear={2022}
+          endYear={null}
+          jobTitle="Fullstack Developer"
+          companyName="FinderUp"
+        />
+        <WorkDetails
+          startYear={2021}
+          endYear={2022}
+          jobTitle="Backend Stack Developer"
+          companyName="Autono.dev"
+        />
+        <div className="hidden lg:block absolute h-full w-0 border-l border-dashed border-white/50" />
+      </div>
+    </div>
+  );
+}
