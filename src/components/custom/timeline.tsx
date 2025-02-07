54cfd299ed7f5ffdca9e2dd80eff5d586815ef4b
Matheus Correia <matheuscorreiags@gmail.com>
2025-02-07 14:42:17 -0300

    feat: view more button

--- src/components/custom/timeline.tsx
+++ src/components/custom/timeline.tsx
@@ -1,36 +1,49 @@
-import { WorkDetails } from "@/components/custom/work-details";
+import {
+  WorkDetails,
+  WorkDetailsProps,
+} from "@/components/custom/work-details";
+import { Button } from "@/components/ui/button";
 
+const jobs: WorkDetailsProps[] = [
+  {
+    startYear: 2022,
+    endYear: null,
+    jobTitle: "Backend Developer in a Angola delivery company (current)",
+    companyName: "Mamboo",
+    imageUrl: "/images/mamboo.jpg",
+  },
+  {
+    startYear: 2022,
+    endYear: null,
+    jobTitle: "Fullstack Developer",
+    companyName: "FinderUp",
+    imageUrl: "/images/saopaulo.webp",
+  },
+  {
+    startYear: 2021,
+    endYear: 2022,
+    jobTitle: "Backend Stack Developer",
+    companyName: "Autono.dev",
+    imageUrl: "/images/joaopessoa.jpg",
+  },
+];
 export function Timeline() {
   return (
-    <div className="flex flex-col text-white py-16 xl:py-32 bg-neutral-950">
+    <div className="flex flex-col text-white py-16 xl:py-32 bg-neutral-950 h-full relative ">
       <h1 className="whitespace-nowrap text-center text-3xl font-bold leading-tight lg:text-[3rem] lg:leading-normal">
         Timeline
       </h1>
 
-      <div className="space-y-8 lg:flex-row lg:gap-x-16 lg:gap-y-0 grid grid-cols-1 place-items-center px-page mt-24 relative">
-        <WorkDetails
-          startYear={2022}
-          endYear={null}
-          jobTitle="Backend Developer in a Angola delivery company (current)"
-          companyName="Mamboo"
-          imageUrl="/images/mamboo.jpg"
-        />
+      <div className="lg:flex-row lg:gap-x-16 lg:gap-y-0 grid grid-cols-1 place-items-center px-page mt-24 relative">
+        {jobs.map((job, index) => (
+          <WorkDetails key={index} isFirst={index === 0} {...job} />
+        ))}
+      </div>
 
-        <WorkDetails
-          startYear={2022}
-          endYear={null}
-          jobTitle="Fullstack Developer"
-          companyName="FinderUp"
-          imageUrl="/images/saopaulo.webp"
-        />
-        <WorkDetails
-          startYear={2021}
-          endYear={2022}
-          jobTitle="Backend Stack Developer"
-          companyName="Autono.dev"
-          imageUrl="/images/joaopessoa.jpg"
-        />
-        <div className="hidden lg:block absolute h-full w-0 border-l border-dashed border-white/50" />
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+        <Button className="h-11 border border-[#fa29bb] hover:border-[#fa29bb]/80">
+          View more
+        </Button>
       </div>
     </div>
   );
--- src/components/custom/work-details.tsx
+++ src/components/custom/work-details.tsx
@@ -1,12 +1,13 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 
-type WorkDetailsProps = {
+export type WorkDetailsProps = {
   startYear: number;
   endYear: number | null;
   jobTitle: string;
   companyName: string;
   imageUrl: string | StaticImport;
+  isFirst?: boolean;
 };
 
 export function WorkDetails({
@@ -15,17 +16,22 @@ export function WorkDetails({
   jobTitle,
   companyName,
   imageUrl,
+  isFirst,
 }: WorkDetailsProps) {
   return (
     <div className="flex flex-col xl:flex-row  w-full items-center justify-center">
-      <div className="flex flex-col lg:flex-row w-full max-w-4xl items-center gap-x-24 ">
-        <Image
-          alt="Work photo"
-          src={imageUrl}
-          width={400}
-          height={500}
-          className="rounded-xl"
-        />
+      <div className="flex flex-col lg:flex-row w-full max-w-4xl gap-x-24">
+        <div className="flex w-full relative items-center justify-center lg:items-start lg:justify-start">
+          <Image
+            alt="Work photo"
+            src={imageUrl}
+            width={400}
+            height={500}
+            className="rounded-xl mt-8"
+          />
+          <div className="hidden lg:block absolute h-full w-0 border-l border-dashed border-white/50 -right-12 mt-36" />
+          <div className="hidden lg:block w-3 h-3 absolute top-0 -right-[54px] rounded-full bg-[#fa29bb] mt-36" />
+        </div>
         <div className="flex flex-col gap-y-4 justify-center items-center lg:justify-end my-16  w-full lg:items-end">
           <h1 className="text-md font-bold text-[#fa29bb]">
             {startYear} - {endYear || "Present"}{" "}
